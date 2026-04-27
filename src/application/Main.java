b91ac63c2857b347af345c24383d4a102bfd88c5
Filipe Rodrigues <filiperodriguesdelima06@gmail.com>
2026-04-27 00:10:05 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "Libraries");
 
-        System.out.println(obj);
+        Seller seller = new Seller(7, "Livy", "livy@gmail.com",
+                new Date(), 4000.0, obj);
+
+        System.out.println(seller);
     }
 }
